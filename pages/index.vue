273cb68875c842a65c5106e4bd7b25e13c108750
keita-f-sato <katanasoulx@gmail.com>
2020-01-19 17:48:09 +0900

    add load page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
   <v-content>
-    <v-container fluid pa-0 fill-height ma-0>
+    <transition name="fade">
+      <v-container v-if="dataLoadFinish === false" fluid pa-0 fill-height ma-0>
+        <v-layout　wrap>
+          <v-flex xs12 sm12 md12 lg12 calss="first-content">
+            <v-card tile flat class="first-card">
+              <v-card-title class="Welcome">Welcome Keita's Portfolio!!</v-card-title>
+              <div v-bind:class="[$vuetify.breakpoint.smAndDown === true ? 'first-bar sm' : 'first-bar']" />
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </transition>
+    <v-container v-if="dataLoadFinish === true" fluid pa-0 fill-height ma-0>
       <v-layout　wrap>
         <v-flex xs12 sm12 md3 lg3 order-lg1 order-sm1 class="About">
             <v-card tile flat class="card" href="/about">
@@ -30,7 +42,7 @@
                     Skill
                     <div class="bar" />
                   <v-card-text class="text text-center">
-                    私のスキルについてです。
+                    スキルについてです。
                   </v-card-text>
                 </v-card-title>
             </v-card>
@@ -55,16 +67,51 @@
 </template>
 
 <script>
-import Topcard from '~/components/TopCard.vue'
-
 export default {
-  components: {
-    Topcard
+  data () {
+        return {
+          dataLoadFinish: false     
+        }
+    },
+  mounted() {
+    setTimeout(() => {
+      this.dataLoadFinish = true
+    }, 2000)
   }
 }
 </script>
 
 <style scoped>
+@import url('assets/move-bar.scss');
+
+.first-card {
+  background-image: linear-gradient(#BD7CF9, #DE5A91);
+  height: 100%;
+}
+
+.Welcome {
+  color: #fff;
+  position: absolute;
+  top: 35%;
+  left: 13%;
+  font-size: 50pt;
+  font-weight: 100;
+}
+
+.expand-transition {
+  transition: all .3s ease;
+  height: 30px;
+  padding: 10px;
+  background-color: #eee;
+  overflow: hidden;
+}
+
+.expand-enter, .expand-leave {
+  height: 0;
+  padding: 0 10px;
+  opacity: 0;
+}
+
 .card {
   position: relative;
   background-image: linear-gradient(#12092C, #12092C);
@@ -130,4 +177,32 @@ a {
   color: #fff !important;
   font-size: 12pt !important;
 }
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .5s
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0
+}
+
+.first-bar {
+  border-bottom: solid 1px ;
+  border-color: #fff;
+  border-image-slice: 1;
+  opacity:0;
+  animation-name:sample02;
+  animation-duration:2s; 
+  animation-fill-mode: forwards;
+  position: relative;
+  top: 45%;
+  left: -20%;
+}
+
+.first-bar.sm {
+  width: 80%;
+}
+
 </style>
