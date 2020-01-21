5273f58d0ddbeabe3d0e5af5f8705ad4329a2aa1
keita-f-sato <katanasoulx@gmail.com>
2020-01-21 23:11:56 +0900

    大学詳細の行間修正

--- pages/work.vue
+++ pages/work.vue
@@ -13,9 +13,7 @@
                   <v-card tile flat class="work">
                   </v-card>
                   <v-card tile flat class="aerospace-card">
-                      <v-card-title v-bind:class="[$vuetify.breakpoint.smAndDown === true ? 'aerospace-title sm' : 'aerospace-title']">
-                      帝京大学 理工学部航空宇宙工学科
-                      </v-card-title>
+                      <v-card-title v-bind:class="[$vuetify.breakpoint.smAndDown === true ? 'aerospace-title sm' : 'aerospace-title']">帝京大学 理工学部航空宇宙工学科</v-card-title>
                       <div class="earo-bar" />
                       <v-card-text v-bind:class="[$vuetify.breakpoint.smAndDown === true ? 'aerospace-day sm' : 'aerospace-day']">
                         - 2016/03
